be4218dad2e374729b44f11ad5ecf2df197e72f8
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-08-27 14:02:51 +0530

    Menu Gsap animations

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,42 @@
 import { useRef, useState } from "react";
 import { allCocktails } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Menu = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const contentRef = useRef();
+
+  useGSAP(() => {
+    gsap.fromTo(
+      "#title",
+      {
+        opacity: 0,
+      },
+      { opacity: 1, duration: 1 }
+    );
+    gsap.fromTo(
+      ".cocktail img",
+      { opacity: 0, xPercent: -100, scale: 1.3 },
+      {
+        xPercent: 0,
+        opacity: 1,
+        duration: 1,
+        scale: 1,
+        ease: "power1.inOut",
+      }
+    );
+    gsap.fromTo(
+      ".details h2",
+      { yPercent: 100, opacity: 0, scale: 1.5 },
+      { opacity: 100, yPercent: 0, scale: 1, ease: "power1.inOut", duration: 1 }
+    );
+    gsap.fromTo(
+      ".details p",
+      { yPercent: 100, opacity: 0 },
+      { opacity: 100, yPercent: 0, ease: "power1.inOut", duration: 1 }
+    );
+  }, [currentIndex]);
 
   const totalCocktails = allCocktails.length;
 
